2c78efa3a4bf05154149c1e99228aeb3b29bfb6b
Michael Tröger <mt@neo-ami.de>
2015-12-13 16:21:04 +0100

    no message

--- KodiDevicePlayer/module.php
+++ KodiDevicePlayer/module.php
@@ -256,9 +256,6 @@ class KodiDevicePlayer extends IPSModule
         if ($Data->DataID <> '{73249F91-710A-4D24-B1F1-A72F216C2BDC}')
             return false;
 
-        if ($Data->Namespace <> self::$Namespace)
-            return false;
-
         if (property_exists($Data, 'Id')) //Reply
         {
             $ReplyJSONDataID = $this->GetIDForIdent('ReplyJSONData');
