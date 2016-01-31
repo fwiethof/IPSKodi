eb96dc9a99ae7144f89748aa40a525c2360551c9
Nall-chan <mt@neo-ami.de>
2016-01-31 17:19:19 +0100

    no message

--- KodiSplitter/module.php
+++ KodiSplitter/module.php
@@ -648,7 +648,7 @@ class KodiSplitter extends IPSModule
      * @param string $ModuleID Die GUID des benötigten Parent.
      * @param string $Name Der Name des Parent, wenn dieser neu angelegt wird.
      */
-    protected function RequireParent(string $ModuleID, string $Name = '')
+    protected function RequireParent(string $ModuleID, string $Name = null)
     {
 
         $instance = IPS_GetInstance($this->InstanceID);
@@ -657,10 +657,10 @@ class KodiSplitter extends IPSModule
 
             $parentID = IPS_CreateInstance($ModuleID);
             $instance = IPS_GetInstance($parentID);
-            if ($Name == '')
-                IPS_SetName($parentID, $instance['ModuleInfo']['ModuleName']);
-            else
+            if ((!is_null($Name)) && ($Name <> ''))
                 IPS_SetName($parentID, $Name);
+            else
+                IPS_SetName($parentID, $instance['ModuleInfo']['ModuleName']);
             IPS_ConnectInstance($this->InstanceID, $parentID);
         }
     }
